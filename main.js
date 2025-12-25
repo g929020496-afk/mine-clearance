5686fd410c0efb212a48d2443331e4226f4d8b93
mmonke <qaz929020496@qq.com>
2025-12-25 16:45:37 +0800

    update

--- main.js
+++ main.js
@@ -12,25 +12,6 @@ Vue.prototype.$backgroundAudioData = {
 	formatedPlayTime: '00:00:00'
 }
 
-import Decimal from "decimal.js";
-const calculate = (a, operator, b) => {
-	const decimalA = new Decimal(a);
-	const decimalB = new Decimal(b);
-	switch (operator) {
-		case '+':
-			return decimalA.plus(decimalB).toString();
-		case '-':
-			return decimalA.minus(decimalB).toString();
-		case '*':
-			return decimalA.times(decimalB).toString();
-		case '/':
-			return decimalA.dividedBy(decimalB).toString();
-		default:
-			throw new Error('Unsupported operator');
-	}
-}
-Vue.prototype.$calculate = calculate;
-
 App.mpType = 'app'
 const app = new Vue({
 	store,
